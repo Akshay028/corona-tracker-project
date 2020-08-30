243af6a02fbd163c6c909aa96b86439d907f0bd4
Akshay Jain <akshay028.jain@gmail.com>
2020-08-30 13:03:38 +0530

    Updates

---
+++ precache-manifest.169746cd69105d0c2c3263abff3a9a44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363029a9c79ac831dac9905b97c5fa61",
+    "url": "/corona-tracker-project/index.html"
+  },
+  {
+    "revision": "d199baac4f8910b0f130",
+    "url": "/corona-tracker-project/static/css/main.7546911c.chunk.css"
+  },
+  {
+    "revision": "4b89077b921042c7d352",
+    "url": "/corona-tracker-project/static/js/2.82da3300.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker-project/static/js/2.82da3300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d199baac4f8910b0f130",
+    "url": "/corona-tracker-project/static/js/main.dadad1bd.chunk.js"
+  },
+  {
+    "revision": "95c3d73c26f971d56e14",
+    "url": "/corona-tracker-project/static/js/runtime-main.d70a6ef0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker-project/static/media/image.d7265326.png"
+  }
+]);
